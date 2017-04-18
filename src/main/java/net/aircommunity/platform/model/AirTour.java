1407398de6e948b36259da8829e729d66aab1639
guankai <Rootroot1>
2017-04-18 19:04:52 +0800

    school 添加tour的旅游路线

--- src/main/java/net/aircommunity/platform/model/AirTour.java
+++ src/main/java/net/aircommunity/platform/model/AirTour.java
@@ -18,115 +18,125 @@ import javax.persistence.Table;
 @Table(name = "air_platfrom_airtour")
 public class AirTour extends Product {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	@Column(name = "city", nullable = false)
-	private String city;
+    @Column(name = "city", nullable = false)
+    private String city;
 
-	@Column(name = "tour_point")
-	private String tourPoint;
+    @Column(name = "tour_point")
+    private String tourPoint;
 
-	@Column(name = "tour_distance")
-	private String tourDistance;
+    @Column(name = "tour_distance")
+    private String tourDistance;
 
-	@Column(name = "tour_time")
-	private String tourTime;
+    @Column(name = "tour_time")
+    private String tourTime;
 
-	@Lob
-	@Column(name = "tour_show")
-	private String tourShow;
+    @Lob
+    @Column(name = "tour_show")
+    private String tourShow;
 
-	@Column(name = "boarding_loc")
-	private String boardingLoc;
+    @Column(name = "boarding_loc")
+    private String boardingLoc;
 
-	@Column(name = "traffic")
-	private String traffic;
+    @Column(name = "traffic")
+    private String traffic;
+    @Column(name = "tour_route")
+    private String tourRoute;
 
-	@OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
-	private Set<AircraftItem> aircraftItems = new HashSet<>();
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
+    private Set<AircraftItem> aircraftItems = new HashSet<>();
 
-	public AirTour() {
-	}
+    public AirTour() {
+    }
 
-	public AirTour(String id) {
-		this.id = id;
-	}
+    public AirTour(String id) {
+        this.id = id;
+    }
 
-	public String getCity() {
-		return city;
-	}
+    public String getCity() {
+        return city;
+    }
 
-	public void setCity(String city) {
-		this.city = city;
-	}
+    public void setCity(String city) {
+        this.city = city;
+    }
 
-	public String getTourPoint() {
-		return tourPoint;
-	}
+    public String getTourPoint() {
+        return tourPoint;
+    }
 
-	public void setTourPoint(String tourPoint) {
-		this.tourPoint = tourPoint;
-	}
+    public void setTourPoint(String tourPoint) {
+        this.tourPoint = tourPoint;
+    }
 
-	public String getTourDistance() {
-		return tourDistance;
-	}
+    public String getTourDistance() {
+        return tourDistance;
+    }
 
-	public void setTourDistance(String tourDistance) {
-		this.tourDistance = tourDistance;
-	}
+    public void setTourDistance(String tourDistance) {
+        this.tourDistance = tourDistance;
+    }
 
-	public String getTourTime() {
-		return tourTime;
-	}
+    public String getTourTime() {
+        return tourTime;
+    }
 
-	public void setTourTime(String tourTime) {
-		this.tourTime = tourTime;
-	}
+    public void setTourTime(String tourTime) {
+        this.tourTime = tourTime;
+    }
 
-	public String getTourShow() {
-		return tourShow;
-	}
+    public String getTourShow() {
+        return tourShow;
+    }
 
-	public void setTourShow(String tourShow) {
-		this.tourShow = tourShow;
-	}
+    public void setTourShow(String tourShow) {
+        this.tourShow = tourShow;
+    }
 
-	public String getBoardingLoc() {
-		return boardingLoc;
-	}
+    public String getBoardingLoc() {
+        return boardingLoc;
+    }
 
-	public void setBoardingLoc(String boardingLoc) {
-		this.boardingLoc = boardingLoc;
-	}
+    public void setBoardingLoc(String boardingLoc) {
+        this.boardingLoc = boardingLoc;
+    }
 
-	public String getTraffic() {
-		return traffic;
-	}
+    public String getTraffic() {
+        return traffic;
+    }
 
-	public void setTraffic(String traffic) {
-		this.traffic = traffic;
-	}
+    public void setTraffic(String traffic) {
+        this.traffic = traffic;
+    }
 
-	public Set<AircraftItem> getAircraftItems() {
-		return aircraftItems;
-	}
+    public Set<AircraftItem> getAircraftItems() {
+        return aircraftItems;
+    }
 
-	public void setAircraftItems(Set<AircraftItem> aircraftItems) {
-		if (aircraftItems != null) {
-			aircraftItems.stream().forEach(item -> item.setProduct(this));
-			this.aircraftItems = aircraftItems;
-		}
-	}
+    public void setAircraftItems(Set<AircraftItem> aircraftItems) {
+        if (aircraftItems != null) {
+            aircraftItems.stream().forEach(item -> item.setProduct(this));
+            this.aircraftItems = aircraftItems;
+        }
+    }
 
-	@Override
-	public String toString() {
-		StringBuilder builder = new StringBuilder();
-		builder.append("AirTour [city=").append(city).append(", tourPoint=").append(tourPoint).append(", tourDistance=")
-				.append(tourDistance).append(", tourTime=").append(tourTime).append(", tourShow=").append(tourShow)
-				.append(", boardingLoc=").append(boardingLoc).append(", traffic=").append(traffic).append(", name=")
-				.append(name).append(", score=").append(score).append(", creationDate=").append(creationDate)
-				.append(", description=").append(description).append(", id=").append(id).append("]");
-		return builder.toString();
-	}
+    public String getTourRoute() {
+        return tourRoute;
+    }
+
+    public void setTourRoute(String tourRoute) {
+        this.tourRoute = tourRoute;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("AirTour [city=").append(city).append(", tourPoint=").append(tourPoint).append(", tourDistance=")
+                .append(tourDistance).append(", tourTime=").append(tourTime).append(", tourShow=").append(tourShow)
+                .append(", boardingLoc=").append(boardingLoc).append(", traffic=").append(traffic).append(", name=")
+                .append(name).append(", score=").append(score).append(", creationDate=").append(creationDate)
+                .append(", description=").append(description).append(", id=").append(id).append("]");
+        return builder.toString();
+    }
 }
--- src/main/java/net/aircommunity/platform/service/internal/AirTourServiceImpl.java
+++ src/main/java/net/aircommunity/platform/service/internal/AirTourServiceImpl.java
@@ -27,95 +27,96 @@ import net.aircommunity.platform.service.AircraftService;
 @Service
 @Transactional
 public class AirTourServiceImpl extends AbstractProductService<AirTour> implements AirTourService {
-	private static final String CACHE_NAME = "cache.airtour";
+    private static final String CACHE_NAME = "cache.airtour";
 
-	@Resource
-	private AirTourRepository airTourRepository;
+    @Resource
+    private AirTourRepository airTourRepository;
 
-	@Resource
-	private AircraftService aircraftService;
+    @Resource
+    private AircraftService aircraftService;
 
-	@Override
-	public AirTour createAirTour(String tenantId, AirTour airTour) {
-		AirTour created = createProduct(tenantId, airTour);
-		addAircraftItems(airTour, created);
-		return airTourRepository.save(created);
-	}
+    @Override
+    public AirTour createAirTour(String tenantId, AirTour airTour) {
+        AirTour created = createProduct(tenantId, airTour);
+        addAircraftItems(airTour, created);
+        return airTourRepository.save(created);
+    }
 
-	@Cacheable(cacheNames = CACHE_NAME)
-	@Override
-	public AirTour findAirTour(String airTourId) {
-		return findProduct(airTourId);
-	}
+    @Cacheable(cacheNames = CACHE_NAME)
+    @Override
+    public AirTour findAirTour(String airTourId) {
+        return findProduct(airTourId);
+    }
 
-	@CachePut(cacheNames = CACHE_NAME, key = "#airTourId")
-	@Override
-	public AirTour updateAirTour(String airTourId, AirTour newAirTour) {
-		return updateProduct(airTourId, newAirTour);
-	}
+    @CachePut(cacheNames = CACHE_NAME, key = "#airTourId")
+    @Override
+    public AirTour updateAirTour(String airTourId, AirTour newAirTour) {
+        return updateProduct(airTourId, newAirTour);
+    }
 
-	@Override
-	protected void copyProperties(AirTour src, AirTour tgt) {
-		tgt.setCity(src.getCity());
-		tgt.setTourDistance(src.getTourDistance());
-		tgt.setBoardingLoc(src.getBoardingLoc());
-		tgt.setTourPoint(src.getTourPoint());
-		tgt.setTourShow(src.getTourShow());
-		tgt.setTourTime(src.getTourTime());
-		tgt.setTraffic(src.getTraffic());
-		tgt.setName(src.getName());
-		tgt.setDescription(src.getDescription());
-	}
+    @Override
+    protected void copyProperties(AirTour src, AirTour tgt) {
+        tgt.setCity(src.getCity());
+        tgt.setTourDistance(src.getTourDistance());
+        tgt.setBoardingLoc(src.getBoardingLoc());
+        tgt.setTourPoint(src.getTourPoint());
+        tgt.setTourShow(src.getTourShow());
+        tgt.setTourTime(src.getTourTime());
+        tgt.setTraffic(src.getTraffic());
+        tgt.setName(src.getName());
+        tgt.setTourRoute(src.getTourRoute());
+        tgt.setDescription(src.getDescription());
+    }
 
-	private void addAircraftItems(AirTour src, AirTour tgt) {
-		Set<AircraftItem> aircraftItems = src.getAircraftItems();
-		if (aircraftItems != null) {
-			aircraftItems.stream().forEach(aircraftItem -> {
-				Aircraft aircraft = aircraftItem.getAircraft();
-				if (aircraft != null) {
-					aircraft = aircraftService.findAircraft(aircraft.getId());
-					aircraftItem.setAircraft(aircraft);
-				}
-			});
-			tgt.setAircraftItems(aircraftItems);
-		}
-	}
+    private void addAircraftItems(AirTour src, AirTour tgt) {
+        Set<AircraftItem> aircraftItems = src.getAircraftItems();
+        if (aircraftItems != null) {
+            aircraftItems.stream().forEach(aircraftItem -> {
+                Aircraft aircraft = aircraftItem.getAircraft();
+                if (aircraft != null) {
+                    aircraft = aircraftService.findAircraft(aircraft.getId());
+                    aircraftItem.setAircraft(aircraft);
+                }
+            });
+            tgt.setAircraftItems(aircraftItems);
+        }
+    }
 
-	@Override
-	public Page<AirTour> listAirTours(int page, int pageSize) {
-		return listAllProducts(page, pageSize);
-	}
+    @Override
+    public Page<AirTour> listAirTours(int page, int pageSize) {
+        return listAllProducts(page, pageSize);
+    }
 
-	@Override
-	public Page<AirTour> listAirTours(String tenantId, int page, int pageSize) {
-		return listTenantProducts(tenantId, page, pageSize);
-	}
+    @Override
+    public Page<AirTour> listAirTours(String tenantId, int page, int pageSize) {
+        return listTenantProducts(tenantId, page, pageSize);
+    }
 
-	@Override
-	public Page<AirTour> listAirToursByCity(String city, int page, int pageSize) {
-		return Pages.adapt(airTourRepository.findByCity(city, Pages.createPageRequest(page, pageSize)));
-	}
+    @Override
+    public Page<AirTour> listAirToursByCity(String city, int page, int pageSize) {
+        return Pages.adapt(airTourRepository.findByCity(city, Pages.createPageRequest(page, pageSize)));
+    }
 
-	@CacheEvict(cacheNames = CACHE_NAME, key = "#airTourId")
-	@Override
-	public void deleteAirTour(String airTourId) {
-		deleteProduct(airTourId);
-	}
+    @CacheEvict(cacheNames = CACHE_NAME, key = "#airTourId")
+    @Override
+    public void deleteAirTour(String airTourId) {
+        deleteProduct(airTourId);
+    }
 
-	@CacheEvict(cacheNames = CACHE_NAME, allEntries = true)
-	@Override
-	public void deleteAirTours(String tenantId) {
-		deleteProducts(tenantId);
-	}
+    @CacheEvict(cacheNames = CACHE_NAME, allEntries = true)
+    @Override
+    public void deleteAirTours(String tenantId) {
+        deleteProducts(tenantId);
+    }
 
-	@Override
-	protected Code productNotFoundCode() {
-		return Codes.TOUR_NOT_FOUND;
-	}
+    @Override
+    protected Code productNotFoundCode() {
+        return Codes.TOUR_NOT_FOUND;
+    }
 
-	@Override
-	protected BaseProductRepository<AirTour> getProductRepository() {
-		return airTourRepository;
-	}
+    @Override
+    protected BaseProductRepository<AirTour> getProductRepository() {
+        return airTourRepository;
+    }
 
 }
